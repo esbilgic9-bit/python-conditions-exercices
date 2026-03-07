0cfbb4c63b66ae0e0448ff6a677f47cbd188360a
Amal Lotfi <esbilgic9@gmail.com>
2026-03-07 22:55:50 +0000

    Add files via upload

---
+++ chair-range-check.py
@@ -0,0 +1,8 @@
+# chair range check 
+chair_number=int(input("Enter your chair number: \n"))
+if chair_number > 21:
+    print("You win")
+elif chair_number < 21:
+    print("You win")
+else:
+    print("You lose")
---
+++ winner-exact-number.py
@@ -0,0 +1,6 @@
+# Winner exact number check
+chair_number=int(input("Enter your chair number: \n"))
+if chair_number == 21:
+    print("congratulations! You are the winner of the party!")
+else:
+    print("Sorry! You lost the game! Better luck next time!")
