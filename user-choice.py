5a198050b6ce955c9eba1bd051febcfd72b57b34
Amal Lotfi <esbilgic9@gmail.com>
2026-03-09 23:45:34 +0000

    Update user-choice.py

---
+++ user-choice.py
@@ -0,0 +1,10 @@
+# User Choice Handling (Yes/No/Maybe)
+user_input = input("Enter a word (yes/no/maybe): \n")
+if user_input == "yes":
+    print("You entered yes !")
+elif user_input == "no":
+    print("You entered no !")
+elif user_input == "maybe":
+    print("You entered maybe !")
+else:
+    print("Comply with the instructions !")
