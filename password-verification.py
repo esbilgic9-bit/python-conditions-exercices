7d9f7fb94792088a5c39b313f058acfd9528058c
Amal Lotfi <esbilgic9@gmail.com>
2026-03-09 23:42:24 +0000

    Update password-verification.py

---
+++ password-verification.py
@@ -0,0 +1,7 @@
+# Password Verification System
+my_pass = "free palestine"
+password_user=input("Enter the password: \n")
+if password_user == my_pass:
+    print("Welcome to the system!")
+else:
+    print("Wrong password! Access denied!")
