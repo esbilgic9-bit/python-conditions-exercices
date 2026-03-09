08d5f6e4c4815c6f5e82a58f28c57506b7b22315
Amal Lotfi <esbilgic9@gmail.com>
2026-03-09 23:37:21 +0000

    Update number-guessing-challenge.py

---
+++ number-guessing-challenge.py
@@ -0,0 +1,6 @@
+# Number Guessing Challenge 
+num = int(input("Please enter a number between 1 and 20: \n"))
+if num == 14:
+    print("Correct ! You've guessed the right number !")
+else:
+    print("Wrong guess ! Try another number ! ")
