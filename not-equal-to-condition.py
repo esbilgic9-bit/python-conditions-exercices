3a965353c996f99d7baf3e4e43924904a29157b2
Amal Lotfi <esbilgic9@gmail.com>
2026-03-09 23:34:00 +0000

    Add not-equal-to-condition.py for game logic

---
+++ not-equal-to-condition.py
@@ -0,0 +1,6 @@
+# Not Equal To Condition  
+chair_number=int(input("Enter your chair number: \n"))
+if chair_number != 21:
+    print("congratulations! You are the winner of the party!")
+else:
+    print("Sorry! You lost the game! Better luck next time!")
